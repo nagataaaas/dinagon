9f3661e137de8c817a84fcdbc887efad8732ed52
Hijiri <hiji.k1124@gmail.com>
2021-11-16 00:13:47 +0900

    jsの加算として正しい値に変更

--- tests/load_fixture.py
+++ tests/load_fixture.py
@@ -4,15 +4,19 @@ from app.utils import hash_password
 
 def load():
     password1, salt1 = hash_password('test1')
-    user1 = User(email_address='test1@test.com', password_hash=password1, salt=salt1)
+    user1 = User(email_address='test1@test.com',
+                 password_hash=password1, salt=salt1)
 
     password2, salt2 = hash_password('test2')
-    user2 = User(email_address='test2@test.com', password_hash=password2, salt=salt2)
+    user2 = User(email_address='test2@test.com',
+                 password_hash=password2, salt=salt2)
 
     session = SessionLocal()
 
-    tag_math = Tag(name='算術理解', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Expressions_and_Operators')
-    tag_func = Tag(name='関数理解', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Functions')
+    tag_math = Tag(
+        name='算術理解', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Expressions_and_Operators')
+    tag_func = Tag(
+        name='関数理解', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Functions')
 
     session.add_all([tag_math, tag_func])
     session.commit()
@@ -20,13 +24,15 @@ def load():
     testcase1 = [
         TestCase(id=None, input='(0, 0)', expected='0'),
         TestCase(id=None, input='(1, 1)', expected='2'),
-        TestCase(id=None, input='(1.0, 1.0)', expected='2.0'),
+        TestCase(id=None, input='(1.0, 1.0)', expected='2'),
         TestCase(id=None, input='(-1, 1)', expected='0'),
         TestCase(id=None, input='(-2, 1)', expected='-1')
     ]
     assertions1 = [
-        Assertion(assertion="'+' in code", message="加算が行われていない可能性があります", tags=[tag_math]),
-        Assertion(assertion="add(0, 0) === undefined", message="値が返却されていない可能性があります", tags=[tag_func])
+        Assertion(assertion="'+' in code",
+                  message="加算が行われていない可能性があります", tags=[tag_math]),
+        Assertion(assertion="add(0, 0) === undefined",
+                  message="値が返却されていない可能性があります", tags=[tag_func])
     ]
 
     testcase2 = [
@@ -37,17 +43,21 @@ def load():
         TestCase(id=None, input='(-2, 1, -1)', expected='-2')
     ]
     assertions2 = [
-        Assertion(assertion="'+' in code", message="加算が行われていない可能性があります", tags=[tag_math]),
-        Assertion(assertion="add(0, 0) === undefined", message="値が返却されていない可能性があります", tags=[tag_func])
+        Assertion(assertion="'+' in code",
+                  message="加算が行われていない可能性があります", tags=[tag_math]),
+        Assertion(assertion="add(0, 0) === undefined",
+                  message="値が返却されていない可能性があります", tags=[tag_func])
     ]
 
     question1 = Question(title='add 2 values',
                          description='2つの値が与えられると、それらの値を足し合わせた数字を返す関数 `add` を定義してください。',
-                         test_cases=testcase1, assertions=assertions1, tags=[tag_math, tag_func],
+                         test_cases=testcase1, assertions=assertions1, tags=[
+                             tag_math, tag_func],
                          default_code="function add(first, second) {\n    // your code here\n\n}")
     question2 = Question(title='add 3 values',
                          description='3つの値が与えられると、それらの値を足し合わせた数字を返す関数 `add` を定義してください。',
-                         test_cases=testcase2, assertions=assertions2, tags=[tag_math, tag_func],
+                         test_cases=testcase2, assertions=assertions2, tags=[
+                             tag_math, tag_func],
                          default_code="function add(first, second, third) {\n    // your code here\n\n}")
 
     session.add_all([user1, user2, *testcase1, *testcase2, *assertions1, *assertions2,
