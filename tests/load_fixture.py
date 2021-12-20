4d79ac8b6fc443635d06e622410cf1deca155758
delta114514 <gey3933@gmail.com>
2021-12-20 22:40:09 +0900

    update fixture add shonagon

--- tests/load_fixture.py
+++ tests/load_fixture.py
@@ -1,5 +1,8 @@
 from app.models import *
 from app.utils import hash_password
+import glob
+import json
+import random
 
 
 def load():
@@ -11,59 +14,51 @@ def load():
 
     session = SessionLocal()
 
-    tag_math = Tag(name='算術理解', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Expressions_and_Operators')
-    tag_func = Tag(name='関数理解', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Functions')
+    tags = [
+        Tag(name='変数', tutorial_link='https://developer.mozilla.org/ja/docs/Learn/JavaScript/First_steps/Variables'),
+        Tag(name='if文',
+            tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Control_flow_and_error_handling#conditional_statements'),
+        Tag(name='算術理解', tutorial_link='https://developer.mozilla.org/ja/docs/Learn/JavaScript/First_steps/Math'),
+        Tag(name='関数定義', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Functions'),
+        Tag(name='関数呼び出し',
+            tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Functions#calling_functions'),
+        Tag(name='定数', tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/const'),
+        Tag(name='関数からの値の返却',
+            tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/return'),
+        Tag(name='for文',
+            tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Loops_and_iteration#for_statement'),
+        Tag(name='型理解',
+            tutorial_link='https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Grammar_and_Types#data_structures_and_types')]
 
-    session.add_all([tag_math, tag_func])
+    session.add_all(tags)
     session.commit()
+    tag_dict = {tag.name: tag for tag in tags}
+    session.add_all([user1, user2])
 
-    testcase1 = [
-        TestCase(id=None, input='(0, 0)', expected='0'),
-        TestCase(id=None, input='(1, 1)', expected='2'),
-        TestCase(id=None, input='(1.0, 1.0)', expected='2.0'),
-        TestCase(id=None, input='(-1, 1)', expected='0'),
-        TestCase(id=None, input='(-2, 1)', expected='-1')
-    ]
-    assertions1 = [
-        Assertion(assertion="'+' in code", message="加算が行われていない可能性があります", tags=[tag_math]),
-        Assertion(assertion="add(0, 0) === undefined", message="値が返却されていない可能性があります", tags=[tag_func])
-    ]
+    for file in glob.glob('./shonagon/assertions/*'):
+        with open(file, 'r', encoding='utf-8') as f:
+            data = json.loads(f.read())
 
-    testcase2 = [
-        TestCase(id=None, input='(0, 0, 0)', expected='0'),
-        TestCase(id=None, input='(1, 1, 1)', expected='3'),
-        TestCase(id=None, input='(1.0, 1.0, 0.5)', expected='2.5'),
-        TestCase(id=None, input='(-1, 1, 0)', expected='0'),
-        TestCase(id=None, input='(-2, 1, -1)', expected='-2')
-    ]
-    assertions2 = [
-        Assertion(assertion="'+' in code", message="加算が行われていない可能性があります", tags=[tag_math]),
-        Assertion(assertion="add(0, 0) === undefined", message="値が返却されていない可能性があります", tags=[tag_func])
-    ]
+        testcases = [TestCase(**case) for case in data['testCase']]
+        assertions = [Assertion(assertion=json.dumps(assertion['assertion']),
+                                message=assertion['message'],
+                                tags=[tag_dict[tag] for tag in assertion['tags']])
+                      for assertion in data['assertions']]
 
-    question1 = Question(title='add 2 values',
-                         description='2つの値が与えられると、それらの値を足し合わせた数字を返す関数 `add` を定義してください。',
-                         test_cases=testcase1, assertions=assertions1, tags=[tag_math, tag_func],
-                         default_code="function add(first, second) {\n    // your code here\n\n}")
-    question2 = Question(title='add 3 values',
-                         description='3つの値が与えられると、それらの値を足し合わせた数字を返す関数 `add` を定義してください。',
-                         test_cases=testcase2, assertions=assertions2, tags=[tag_math, tag_func],
-                         default_code="function add(first, second, third) {\n    // your code here\n\n}")
+        question = Question(title=data['title'], description=data['description'],
+                            test_cases=testcases, assertions=assertions, tags=[tag_dict[tag] for tag in data['tags']],
+                            default_code=data['defaultCode'])
 
-    session.add_all([user1, user2, *testcase1, *testcase2, *assertions1, *assertions2,
-                     question1, question2])
-    session.commit()
+        session.add_all([*testcases, *assertions, question])
+        session.commit()
 
-    answers = [
-        Answer(user=user1.id, question=question1.id, is_correct=False),
-        Answer(user=user1.id, question=question1.id, is_correct=True),
-        Answer(user=user1.id, question=question2.id, is_correct=True),
-        Answer(user=user2.id, question=question1.id, is_correct=True),
-        Answer(user=user2.id, question=question2.id, is_correct=False),
-        Answer(user=user2.id, question=question2.id, is_correct=True),
-    ]
-
-    session.add_all(answers)
+        for user in [user1, user2]:
+            is_correct = random.choice([True, False])
+            if is_correct:
+                wrong = []
+            elif assertions:
+                wrong = random.sample(assertions, random.randint(1, len(assertions)))
+            session.add(Answer(user=user.id, question=question.id, is_correct=False, failed_assertions=wrong))
     session.commit()
     session.close()
 
