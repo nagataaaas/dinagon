8df9987988deee2c1033ed5d717fbe3ef6a72c0b
delta114514 <gey3933@gmail.com>
2022-01-09 04:20:06 +0900

    update cors

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['https://chunagon.herokuapp.com'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
