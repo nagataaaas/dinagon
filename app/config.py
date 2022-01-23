0b1a9c3b5d9a669a36abccda4af52ded431df688
delta114514 <gey3933@gmail.com>
2022-01-24 02:51:31 +0900

    change database-uri

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 IS_LOCAL = os.path.exists('.LOCAL')
 IS_DEV = True
 HTML_DIR = 'app/templates'
-DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+DATABASE_URI = 'sqlite:///database.db'
 
 HOST = '0.0.0.0'
 PORT = int(os.environ.get('PORT', 8888))
