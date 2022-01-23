752fcf15a30976d45927cf42ef162ccc8461c73e
delta114514 <gey3933@gmail.com>
2022-01-24 03:01:39 +0900

    add localhost to allow_origin

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://chunagon.herokuapp.com', 'https://chunagon-dash.herokuapp.com'],
+    allow_origins=['https://chunagon.herokuapp.com', 'https://chunagon-dash.herokuapp.com', 'http://localhost:3000'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
