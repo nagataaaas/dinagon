1818180326dfac10fb1da848f38c9ca85ea3a40d
delta114514 <gey3933@gmail.com>
2022-01-24 02:51:06 +0900

    add chunagon-dash

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://chunagon.herokuapp.com'],
+    allow_origins=['https://chunagon.herokuapp.com', 'https://chunagon-dash.herokuapp.com'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
